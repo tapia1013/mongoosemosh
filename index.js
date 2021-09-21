136d7e6c1d4deac0e8455e635d063988923d33cb
tapia1013 <manbearpig1013@gmail.com>
2021-09-20 19:31:21 -0700

    mongoose operators and logical operators

--- index.js
+++ index.js
@@ -51,25 +51,51 @@ async function getCourses() {
    * lte (less than or equal to)
    * in
    * nin (not in)
+   * 
+   * 
+   *           LOGIC OPERATORS
+   * or
+   * and
+   * 
    */
+
+  // for skip pagination
+  const pageNumber = 2;
+  const pageSize = 10;
+  // /api/course?pageNumber=2&pageSize=10
 
 
   const courses = await Course
-    // .find({ author: 'Vee', isPublished: true })
+    .find({ author: 'Vee', isPublished: true })
     // get all courses that are greater than $10
     // .find({ price: { $gte: 10, $lte: 20 } })
     // for multiple values we use $in
-    .find({ price: { $in: [10, 15, 20] } })
-    .limit(10)
+    // .find({ price: { $in: [10, 15, 20] } })
+    // Logic Operators
+    // .find()
+    // .or([{ author: 'Mosh' }, { isPublished: true }])
+    // .and([{}])
+    // regex
+    // .find({ author: /^Jay/ })
+    // author ends with string
+    // .find({ author: /Vee$/ })
+    // author contains vee
+    // .find({ author: /.*Vee.*/i })
+
+    // SKIP IS FOR PAGINATION
+    .skip((pageNumber - 1) * pageSize)
+    .limit(pageSize)
     .sort({ name: 1 })
     .select({ name: 1, tag: 1 })
+    // Count
+    .count() // 2
   console.log(courses);
 }
 getCourses()
 
 
 
-
+// 14.Excerise.1
 
 
 
